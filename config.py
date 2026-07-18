bb49cdf307b45bc81f83294c763444af6cfb68bf
yhungsammy57-max <yhungsammy57@gmail.com>
2026-07-18 19:27:34 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = "123456789:AA..."
+
+          
+
+ADMIN_ID = 7524997387
+
+WHATSAPP_CHANNEL = "https://whatsapp.com/channel/0029Vb7RUka3QxRuxsBqjV2i"
+BOT_NAME = "EASY2CASHBOT"
